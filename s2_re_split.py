85b3392c51e67260c49a7beb73711a2d6ebf18d8
yym <583692365@qq.com>
2020-01-08 16:55:28 +0800

    把@的好友split并for循环输出

--- s2_re_split.py
+++ s2_re_split.py
@@ -1,6 +1,17 @@
 # author yym
 import re
+
 pattern = r'[?|&]'  # 只能用中括号，用小括号不好使
 url = "https://www.mingrikeji?beijingshengongwuyazi&zhangwuji&hongqigong"
 result = re.split(pattern, url)
 print(result)
+
+# 把艾特的好友都表示出来
+pattern = r'\s*@'  # \s表示空字符  *表示限定符
+list1 = "@明日科技 @令狐葱 @神雕侠 @小龙女 @无崖子"
+result = re.split(pattern, list1)
+print("您的好友如下：")
+for item in result:
+    if item != "":
+        print(item)
+
