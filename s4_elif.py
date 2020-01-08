9fadaca01fe700575260347e1a8382a6c15b5631
yym <583692365@qq.com>
2020-01-08 18:40:33 +0800

    elif玫瑰花语

---
+++ s4_elif.py
@@ -0,0 +1,15 @@
+# author yym
+print("玫瑰花语朵，请输入你想送几朵玫瑰花：")
+num = int(input("请输入你想朵玫瑰花："))
+if num == 1:
+    print("你是我的唯一！")
+elif num == 3:
+    print("I love you")
+elif num == 10:
+    print("十全十美！！！")
+elif num == 99:
+    print("天长地久！！！")
+elif num == 108:
+    print("求婚！！")
+else:
+    print("小么也不知道这个是什么意思了，哈哈哈哈")
